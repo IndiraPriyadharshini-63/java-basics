add33e0459705a3bb2db847f9d6fcb9325098c1b
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2025-03-07 22:43:19 +0530

    added enum

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -5,11 +5,15 @@ public class App {
         byte b = 10;
         short s = 1000;
         int num = 123456789;
-        long bigNum = 31234567896548L; // it shows error for long numbers, so we have to add L to denote long number
+        long bigNum = 31234567896548L;
         float pi = 3.14F;
         double area = 12.3456;
         char letter = 'A';
         boolean isEligible = false;
+        enum Day {
+            SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+        }
+        Day today = Day.MONDAY;
 
         System.out.println("Byte: " + b);
         System.out.println("Short: " + s);
@@ -19,5 +23,6 @@ public class App {
         System.out.println("Double: " + area);
         System.out.println("Character: " + letter);
         System.out.println("Boolean: " + isEligible);
+        System.out.println("Today:  " + today);
     }
 }
