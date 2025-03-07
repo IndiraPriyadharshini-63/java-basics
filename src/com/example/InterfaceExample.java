e2a12daf8609a288b29786cf4a5858b0e3edd06c
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2025-03-07 10:19:46 +0530

    Interface

---
+++ src/com/example/InterfaceExample.java
@@ -0,0 +1,20 @@
+package com.example;
+
+interface Polygon {
+      void getArea(int length, int breadth);
+
+}
+
+class Rectangle implements Polygon {
+      public void getArea(int length, int breadth) {
+            System.out.println("The area of the rectangle is " + (length * breadth));
+      }
+}
+
+public class InterfaceExample {
+      public static void main(String[] args) {
+            Rectangle r = new Rectangle();
+            r.getArea(5, 6);
+      }
+
+}
